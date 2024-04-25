e2622c4f448b1c2405427539526780e5d85e7b35
Rodrigogalo1 <rodrigo.galo96@gmail.com>
2024-04-24 22:52:02 -0400

    actualizacion 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
 import './App.css'
-import { Form } from './components/Formulario'
+
 import { Registro } from './components/Registro'
 
 function App() {
@@ -8,15 +7,9 @@ function App() {
 
   return (
     <> 
-    <div className="card formulario">
-      <div className="card-body">
-    <h1>Crea una cuenta</h1>
+
     <Registro/>
-    <h5>O usa tu mail para registrarte</h5>
-     <Form />
-    <alert/>
-     </div>
-    </div>
+
     </>
   )
 }
--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
 import { Button } from './SocialButton'; 
+import { Form } from './Formulario'
+
+
+let icons ={
+    facebook: 'fab fa-facebook-f',
+    google: 'fab fa-google',
+    twitter: 'fab fa-twitter',
+    github: 'fab fa-github'
+
+}
 
 export const Registro = () => {
     return (
-            <Button />
+        <>
+        
+        <div className="card formulario">
+        <div className="card-body">
+        <h1>Crea una cuenta</h1>
+            <Button icons={icons} />
+        <h5>O usa tu mail para registrarte</h5>
+        <Form />
+        <alert/>
+        </div>
+        </div>
+        
+        </>
     )
 }
